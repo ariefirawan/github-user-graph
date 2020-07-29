3e0b390c23099004a85402b566132af3f183b0a0
arief irawan <ariefirawant@gmail.com>
2020-07-30 05:44:56 +0700

    done doughnut2d chart

--- src/components/Charts/Doughnut2d.js
+++ src/components/Charts/Doughnut2d.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import ReactFC from 'react-fusioncharts';
+import FusionCharts from 'fusioncharts';
+import Column2D from 'fusioncharts/fusioncharts.charts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.candy';
 
-const Doughnut2d = () => {
-  return <div>chart</div>;
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+
+const Doughnut2d = ({ data }) => {
+  const chartConfigs = {
+    type: 'doughnut2d', // The chart type
+    width: '100%', // Width of the chart
+    height: '400', // Height of the chart
+    dataFormat: 'json', // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        //Set the chart caption
+        caption: 'Stars Per Language',
+        decimals: 0,
+        doughnutRadius: '45%',
+        showPercentValues: 0,
+        theme: 'candy',
+      },
+      // Chart Data
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
 };
 
 export default Doughnut2d;
