b350328b83f7ade95ab76d5a1c380cbb0f533487
arief irawan <ariefirawant@gmail.com>
2020-07-29 14:11:35 +0700

    modify chart pie3D component

--- src/components/Charts/Bar3D.js
+++ src/components/Charts/Bar3D.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import ReactFC from 'react-fusioncharts';
+import FusionCharts from 'fusioncharts';
+import Column2D from 'fusioncharts/fusioncharts.charts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 
-const Bar3D = () => {
-  return <div>chart</div>;
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+
+const Bar3D = ({ data }) => {
+  const chartConfigs = {
+    type: 'pie3d', // The chart type
+    width: '400', // Width of the chart
+    height: '400', // Height of the chart
+    dataFormat: 'json', // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        //Set the chart caption
+        caption: 'Languages',
+        theme: 'fusion',
+        decimals: 0,
+        pieRadius: '45%',
+      },
+      // Chart Data
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
 };
 
 export default Bar3D;
